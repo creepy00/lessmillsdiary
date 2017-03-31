d500e48c4c647a46d992b47c0ac6d1e54b81713a
Goran Blazin <goran.blazin@gmail.com>
2017-04-01 00:43:05 +0200

    Adding js and css endpoints Adding bootstrap and jquery npm

--- server.go
+++ server.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"net/http"
 	"log"
 )
 
-func responseHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "front/index.html")
+var port string
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[1:]
+	log.Println("Enpoint called: " + path)
+	http.ServeFile(w, r, "front/" + path)
+}
+
+func handleJs(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[4:]
+	log.Println("Enpoint called to JS: " + path)
+	http.ServeFile(w, r, "node_modules/jquery/dist/" + path)
+	http.ServeFile(w, r, "node_modules/bootstrap/dist/js/" + path)
+}
+
+func handleCss(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[5:]
+	log.Println("Enpoint called to CSS: " + path)
+	http.ServeFile(w, r, "node_modules/bootstrap/dist/css/" + path)
 }
 
 func main() {
-	http.HandleFunc("/", responseHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port = "8080"
+
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/js/", handleJs)
+	http.HandleFunc("/css/", handleCss)
+
+	fmt.Println("Server started on port " + port)
+
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+
 }
